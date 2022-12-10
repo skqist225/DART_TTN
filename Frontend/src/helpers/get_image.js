7f9164463bd99c6c68dbe782776dc133908faf85
Phan Dinh Khai <pdkhai@tma.com.vn>
2022-12-10 12:59:42 +0700

    fix refresh error netlify

--- Frontend/src/helpers/get_image.js
+++ Frontend/src/helpers/get_image.js
@@ -1,3 +1,7 @@
 export default function getImage(imageName) {
-    return `http://localhost:8080${imageName}`;
+    return `${
+        import.meta.env.VITE_NODE_ENV === "development"
+            ? import.meta.env.VITE_LOCAL_SERVER_URL + `${imageName}`
+            : import.meta.env.VITE_REMOTE_SERVER_URL + `${imageName}`
+    }`;
 }
